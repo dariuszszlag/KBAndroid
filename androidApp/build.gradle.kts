3b2317058d0c901b75998ddce1485374b73d750c
Dariusz Szlag <dariusz.szlag@appunite.com>
2023-04-20 18:08:04 +0200

    Fix lazycolumns in kbembed

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 
 dependencies {
     implementation("com.darek:kbcore-android:0.0.17")
-    implementation("com.darek:kbembed:0.0.1")
+    implementation("com.darek:kbembed:0.0.3")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling")
