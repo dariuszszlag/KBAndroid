2a0a19f24ec000d386cb054b9a66c13334f5d8db
dariuszszlag <99176959+dariuszszlag@users.noreply.github.com>
2023-04-20 18:08:23 +0200

    Fix lazycolumns in kbembed

--- kbembed/src/main/java/com/darek/kbembed/ui/screens/posts/PostsScreen.kt
+++ kbembed/src/main/java/com/darek/kbembed/ui/screens/posts/PostsScreen.kt
@@ -4,6 +4,7 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
